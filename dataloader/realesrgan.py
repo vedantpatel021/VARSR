7c9a10a5f3012d4e8f5fdb8bcdf42a5e3061ac55
Vedant Patel <93401496+vedantpatel021@users.noreply.github.com>
2025-11-11 20:12:04 -0500

    Change scale_final value from 10 to 4

--- dataloader/realesrgan.py
+++ dataloader/realesrgan.py
@@ -197,7 +197,7 @@ class RealESRGAN_degradation(object):
         ori_h, ori_w = img_gt.size()[2:4]
 
         #scale_final = random.randint(4, 16)
-        scale_final = 10
+        scale_final = 4
 
         # ----------------------- The first degradation process ----------------------- #
         # blur
